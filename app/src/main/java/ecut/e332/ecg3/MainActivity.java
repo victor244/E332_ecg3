c9ceecc9c58cf1dfe5a0541b62ac73dfb3cfc9f2
victor244 <v153351@gmail.com>
2020-01-17 10:58:11 +0800

    first commit

---
+++ app/src/main/java/ecut/e332/ecg3/MainActivity.java
@@ -0,0 +1,14 @@
+package ecut.e332.ecg3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
